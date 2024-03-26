df08e23196d7708665f9d3b57480f8a22acbd8c0
purekkim <165023839+purekkim@users.noreply.github.com>
2024-03-26 20:40:01 +0900

    Title: 가장 짧은 문자열, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/가장 짧은 문자열/shortest-string.py
+++ 240326/가장 짧은 문자열/shortest-string.py
@@ -1,15 +1,15 @@
 # 세 개의 문자열 입력 받기
-string1 = input()
-string2 = input()
-string3 = input()
+str1 = input()
+str2 = input()
+str3 = input()
 
 # 문자열 길이 차 계산 함수 정의
 def find_length_difference(str1, str2, str3):
     lengths = [len(str1), len(str2), len(str3)]
     mx = max(lengths)
     mn = min(lengths)
-    difference = max_length - min_length
+    difference = mx-mn
     return difference
 
 # 함수 호출하여 결과 출력
-print(find_length_difference(string1, string2, string3))
+print(find_length_difference(str1, str2, str3))
