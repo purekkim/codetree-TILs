502cc5709f5de61093f87652572ef78b7ff773bf
purekkim <165023839+purekkim@users.noreply.github.com>
2024-03-29 19:47:29 +0900

    Title: 숫자로 이루어진 사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240329/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,17 @@
+def solution():
+    row_num = int(input())
+    return row_num
+
+def solution():
+    n = int(input())
+    num = input()
+    return n num
+
+# n개의 줄에 걸쳐 특정 문자열을 출력하는 함수입니다.
+def print_num(n,num):  
+    cnt=0
+    for _ in num:
+        print(cnt,end="")
+        cnt+=1
+    
+print_num(solution())
