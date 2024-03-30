77147cb437b39109b588ffd0029a30f7a3b5ccbb
purekkim <165023839+purekkim@users.noreply.github.com>
2024-03-30 09:55:23 +0900

    Title: 배열의 특정 위치 찾기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240330/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240330/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,5 +1,16 @@
 arr=list(map(int,input().split()))
 
-len_arr=len(arr)
+n=len(arr)
+
+cnt=0
+sum_even=0
 sum_val=0
-for i in range
+for i in range(1,n,2):
+    sum_even += arr[i]
+for i in range(2,n,3):
+    sum_val += arr[i]
+    cnt += 1
+    avg=sum_val/cnt
+
+
+print(sum_even,f"{avg:.1f}")
